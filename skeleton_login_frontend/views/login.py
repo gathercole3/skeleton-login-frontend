eb5110a6831fe0a38ef9db3fb6c27cfbe991b7a4
jacob gathercole <jacobgathercole@gmail.com>
2017-05-29 01:12:52 +0100

    added route to receave google token to post to api

--- skeleton_login_frontend/views/login.py
+++ skeleton_login_frontend/views/login.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 login = Blueprint('login', __name__)
 
@@ -6,3 +6,8 @@ login = Blueprint('login', __name__)
 @login.route("/login")
 def display_login_page():
     return render_template('pages/display_logins.html')
+
+@login.route("/login/oauthGoogleValidate", methods=['POST'])
+def validate_google_token():
+    post_data = request.form
+    return post_data['authorization_grant']
