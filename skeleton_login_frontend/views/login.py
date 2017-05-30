3cb67c021a555853abbeca2d864dd535d950f30d
jacob gathercole <jacobgathercole@gmail.com>
2017-05-30 20:07:59 +0100

    added code to send token to api and return responce

--- skeleton_login_frontend/views/login.py
+++ skeleton_login_frontend/views/login.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, current_app, jsonify
+import requests
+import json
 
 login = Blueprint('login', __name__)
 
@@ -10,4 +12,11 @@ def display_login_page():
 @login.route("/login/oauthGoogleValidate", methods=['POST'])
 def validate_google_token():
     post_data = request.form
-    return post_data['authorization_grant']
+
+    url = "http://skeleton_login_api:8000/verify_google_token"
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    payload = {}
+    payload["token"] = post_data['authorization_grant']
+    response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+
+    return response.text
