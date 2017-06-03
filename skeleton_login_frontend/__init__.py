d78de454d3e9874a225079e45059dbb8d89ea470
jacob gathercole <jacobgathercole@gmail.com>
2017-06-03 14:34:31 +0100

    added exception handler and code to attempt to log a user in

--- skeleton_login_frontend/__init__.py
+++ skeleton_login_frontend/__init__.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 app.config.from_pyfile("config.py")
 
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
 #these imports must be included after the app object has been created as it is imported in them
 from skeleton_login_frontend.blueprints import register_blueprints
+from skeleton_login_frontend.exceptions import register_exception_handlers
 
+register_exception_handlers(app)
 register_blueprints(app)
--- skeleton_login_frontend/views/login.py
+++ skeleton_login_frontend/views/login.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, current_app, jsonify
+from flask import Blueprint, render_template, request, current_app, jsonify, session
 import requests
 import json
+from skeleton_login_frontend.exceptions import ApplicationError
 
 login = Blueprint('login', __name__)
 
@@ -8,6 +9,10 @@ login = Blueprint('login', __name__)
 @login.route("/login")
 def display_login_page():
     return render_template('pages/display_logins.html')
+
+@login.route("/login/oauth")
+def display_oauth_login_page():
+    return render_template('pages/display_oauth_logins.html')
 
 @login.route("/login/oauthGoogleValidate", methods=['POST'])
 def validate_google_token():
@@ -18,5 +23,39 @@ def validate_google_token():
     payload = {}
     payload["token"] = post_data['authorization_grant']
     response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+    json_data = json.loads(response.text)
+
+    session['user-id'] = json_data['id']
+    session['google_login'] = True
 
     return response.text
+
+@login.route("/login/validate_login", methods=['POST'])
+def validate_login():
+    post_data = request.form
+
+    url = current_app.config["LOGIN_API_URL"] + "/verify_login"
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    payload = {}
+    payload["email"] = post_data['email']
+    payload["password"] = post_data['password']
+    response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+
+    json_data = json.loads(response.text)
+
+    if response.status_code != 200:
+
+        #code u001 has been specified to be an incorrect email and password combination so we should check for this
+        if json_data['error_code'] == 'u001':
+            return "incorrect email and password combo"
+
+        raise ApplicationError("something has gone wrong trying to log you in", 'unspecified')
+    session['user-id'] = json_data['id']
+    session['google_login'] = True
+
+    return response.text
+
+@login.route("/logout")
+def display_logout_page():
+    session.clear()
+    return render_template('pages/display_logout.html', googleLogin=googleLogin)
